5a029f9324527dc641462de7bbe0c973d3b3c2a6
Tomasz Smiechowski <palczety@MacBook-Pro-Tomasz.local>
2020-02-24 15:49:30 +0100

    teraz bedzie git

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"; 
+import "./App.css";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <li>TO DO APP</li>;
+  }
+}
+
+export default App;
